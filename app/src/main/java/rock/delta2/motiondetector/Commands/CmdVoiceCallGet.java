7a74c2af8f1e54807394d82bf0f509ccba1eac40
ruslan <sjrk@ukr.net>
2019-01-28 03:00:11 -0500

    next ....

--- app/src/main/java/rock/delta2/motiondetector/Commands/CmdVoiceCallGet.java
+++ app/src/main/java/rock/delta2/motiondetector/Commands/CmdVoiceCallGet.java
@@ -15,7 +15,7 @@ public class CmdVoiceCallGet extends CmdBase {
 
     public String getDescription(Context context){
         return String.format("\n%s - %s", _COMMAND
-                , context.getResources().getString(R.string.cmd_delta_set_description));
+                , context.getResources().getString(R.string.cmd_voice_set_description));
     }
 
     public CmdVoiceCallGet(){
